1acc7a95b2f5e28ee684ee6578f9ecf4e0f5533f
Mohammad AlMahllawi <mohammadalmahllawi@gmail.com>
2025-09-20 23:17:59 +0300

    Modify scripts

--- run.js
+++ run.js
@@ -3,7 +3,13 @@ import { execSync } from "node:child_process";
 switch (process.argv[2]) {
 	case "migration:generate":
 		execSync(
-			`pnpm typeorm migration:generate ./src/migrations/${process.argv[3] ?? "migration"}`,
+			`pnpm typeorm migration:generate --dataSource ./src/datasource.ts ./src/migrations/${process.argv[3] ?? "migration"}`,
+			{ stdio: [0, 1, 2] },
+		);
+		break;
+	case "entity:create":
+		execSync(
+			`pnpm typeorm entity:create ./src/entities/${process.argv[3] ?? "entity"}.entity`,
 			{ stdio: [0, 1, 2] },
 		);
 		break;
