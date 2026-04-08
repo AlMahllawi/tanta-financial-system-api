b7f86e3d5ad109a546f0b8de59dd30b13969f4e1
Mohammad AlMahllawi <mohammadalmahllawi@gmail.com>
2026-04-08 14:30:25 +0200

    feat: override handleRequest in JwtAuthGuard to throw custom ApiException for unauthorized access

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpStatus } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiException } from '../../common/exceptions/api.exception.js';
+import { ErrorCode } from '../../common/enums/error-codes.enum.js';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest<TUser = unknown>(err: unknown, user: unknown): TUser {
+    if (err || !user)
+      throw err instanceof Error
+        ? err
+        : new ApiException(HttpStatus.UNAUTHORIZED, ErrorCode.UNAUTHORIZED);
+
+    return user as TUser;
+  }
+}
