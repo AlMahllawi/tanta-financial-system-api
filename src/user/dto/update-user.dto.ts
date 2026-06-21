77cf1a45814a2351fd898b8c72d9445426eaba1d
Mohammad AlMahllawi <mohammadalmahllawi@gmail.com>
2026-06-21 16:08:44 +0300

    fix(user/dto): fix UserRole enum import path

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,7 +1,7 @@
 import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { IsBoolean, IsOptional } from 'class-validator';
-import { UserRole } from 'prisma/generated/enums.js';
 
+import { UserRole } from '../../../prisma/generated/enums.js';
 import { CreateUserDto } from './create-user.dto.js';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
